21c255b8a8593eea4f7bd298c9c659794226573f
PatcharapolS <64309010026@chontech.ac.th>
2021-12-02 16:23:12 +0700

    demo

--- showdemo.php
+++ showdemo.php
@@ -17,7 +17,7 @@ $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
   // output data of each row
   while($row = mysqli_fetch_assoc($result)) {
-    echo "id: " . $row["id"]. " - Name: " . $row["name"]. " " . $row["addrech"]. "<br>";
+    echo "id: " . $row["id"]. " - Name: " . $row["name"]. " Address " . $row["address"]. "<br>";
   }
 } else {
   echo "0 results";
